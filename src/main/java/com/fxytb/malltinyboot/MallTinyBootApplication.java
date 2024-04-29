a8f410753988a3a17583da6662fe6df489e6f019
fuxinyu <cfhxinya@gmail.com>
2024-04-29 11:28:55 +0800

    mall-tiny-boot项目初始化

---
+++ src/main/java/com/fxytb/malltinyboot/MallTinyBootApplication.java
@@ -0,0 +1,13 @@
+package com.fxytb.malltinyboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallTinyBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallTinyBootApplication.class, args);
+    }
+
+}
